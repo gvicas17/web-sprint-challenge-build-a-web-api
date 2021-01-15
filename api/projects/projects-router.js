044da5b258c8048cc6795f95d2b019b1ab06cdb7
Gabby Vicas <68706544+gvicas17@users.noreply.github.com>
2021-01-15 12:57:07 -0500

    created projects endpoints with a few bugs

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -28,4 +28,47 @@ router.get('/:id', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    Projects.insert(req.body)
+    .then(newProject=> {
+        if(!newProject.name || !newProject.description){
+            res.status(400).json({message: 'please include an name & description'})
+        }else{
+            res.status(202).json(newProject)
+        }
+    })
+    .catch(error => {
+        res.status(500).json({message: 'there was an error while trying to create new project'})
+    })
+})
+
+router.put('/:id', (req, res) => {
+    Projects.update(req.params.id, req.body)
+    .then(project=> {
+        if(!project.name|| !project.description){
+            res.status(400).json({message: 'please include project name & description'})
+        }else{
+        res.status(200).json(project)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error updating project'
+        })
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    Projects.remove(req.params.id)
+    .then(project=> {
+        if (!project){
+            res.status(400).json({message: 'The project with this id does not exist'})
+        }else{
+            res.status(200).json({message: 'project successfully deleted'})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: 'something went wrong trying to delete project'})
+    })
+})
 module.exports = router
